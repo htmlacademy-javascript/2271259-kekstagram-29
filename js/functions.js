499105d79e0baea1b3591862b2f0932c267c71e7
Kirill Mezentsev <kirmez02@gmail.com>
2023-06-19 11:30:06 +0200

    corrected objects

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,17 @@
 
-const numberOfCharacters = function(sentence, num) {
-  if (sentence.length <= num) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// const numberOfCharacters = function(sentence, num) {
+//   if (sentence.length <= num) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-numberOfCharacters();
+// numberOfCharacters();
 
-const palindrome = function(str) {
-  return str === str.split('').reverse().join('');
-};
+// const palindrome = function(str) {
+//   return str === str.split('').reverse().join('');
+// };
 
 
-palindrome();
+// palindrome();
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const names = [
   'John',
   'Jack',
@@ -11,7 +12,7 @@ const names = [
   'Leo'
 ];
 
-const comments = [
+const messages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
@@ -21,47 +22,10 @@ const comments = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const photos = [
-  '1.jpg',
-  '2.jpg',
-  '3.jpg',
-  '4.jpg',
-  '5.jpg',
-  '6.jpg',
-  '7.jpg',
-  '8.jpg',
-  '9.jpg',
-  '10.jpg',
-  '11.jpg',
-  '12.jpg',
-  '13.jpg',
-  '14.jpg',
-  '15.jpg',
-  '16.jpg',
-  '17.jpg',
-  '18.jpg',
-  '19.jpg',
-  '20.jpg',
-  '21.jpg',
-  '22.jpg',
-  '23.jpg',
-  '24.jpg',
-  '25.jpg'
-];
-
-const avatars = [
-  'avatar-1.svg',
-  'avatar-2.svg',
-  'avatar-3.svg',
-  'avatar-4.svg',
-  'avatar-5.svg',
-  'avatar-6.svg'
-];
-
 const similarPersonCount = 25;
 
 const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.cell(Math.min(Math.abs(a), Math.abs(b)));
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
@@ -69,21 +33,34 @@ const getRandomPositiveInteger = (a, b) => {
 
 const getRandomArrayElement = (elements) => (elements[getRandomPositiveInteger(0, elements.length - 1)]);
 
-
-function createPerson () {
-  return {
-    id: Math.random(1,25),
-    url: getRandomArrayElement(photos),
-    avatar: getRandomArrayElement(avatars),
-    description: 'My avatar',
-    likes: Math.random(15, 200),
-    message: getRandomArrayElement(comments),
-    name: getRandomArrayElement(names),
+const getId = () => {
+  let index = 0;
+  return () => {
+    index++;
+    return index;
   };
-}
+};
 
-const similarPerson = Array.from({length: similarPersonCount}, createPerson);
+const generatePhotoId = getId(1, 25);
+const generateCommentId = getId(1, 750);
 
-similarPerson();
+const generateComments = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+  message: getRandomArrayElement(messages),
+  name: getRandomArrayElement(names)
 
+});
 
+const createPerson = () => ({
+
+  id: generatePhotoId(),
+  url: `photos/${getRandomPositiveInteger(1, 25)}.jpg`,
+  description: 'My photo',
+  likes: Math.floor(Math.random() * 100),
+  comments: generateComments(),
+});
+
+const createSimilarPerson = Array.from({length: similarPersonCount}, createPerson, () => new createPerson());
+
+createSimilarPerson();
